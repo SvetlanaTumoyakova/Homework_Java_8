53874638c8fac07682157f954381222e36e4c7ef
Светлана Тумоякова <Sveta.Tumoyakova@yandex.ru>
2026-01-20 01:10:16 +0700

    update project

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,9 @@
 public class Main {
-    enum Transports{
-        CAR,
-        TRUCK,
-        AIRPLANE,
-        TRAIN,
-        BOAT
-    }
     public static void main(String[] args) {
+        System.out.println("Скорость автомобиля составляет " + Transports.CAR.getSpeed() + " миль в час.");
+        System.out.println("Скорость грузовика составляет " + Transports.TRUCK.getSpeed() + " миль в час.");
+        System.out.println("Скорость самолета составляет " + Transports.AIRPLANE.getSpeed() + " миль в час.");
+        System.out.println("Скорость поезда составляет " + Transports.TRAIN.getSpeed() + " миль в час.");
+        System.out.println("Скорость катера составляет " + Transports.BOAT.getSpeed() + " миль в час.");
     }
 }
--- src/Transports.java
+++ src/Transports.java
@@ -1,3 +1,18 @@
 public enum Transports {
+    CAR (65),
+    TRUCK(55),
+    AIRPLANE(600),
+    TRAIN(70),
+    BOAT(22);
+
+    private final int speed;
+
+    Transports(int speed) {
+        this.speed = speed;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
 
 }
