3a09c0b0500d11007dc80dcf0032a8c8e25cf452
Светлана Тумоякова <Sveta.Tumoyakova@yandex.ru>
2026-01-20 00:58:19 +0700

    init project

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    enum Transports{
+        CAR,
+        TRUCK,
+        AIRPLANE,
+        TRAIN,
+        BOAT
+    }
+    public static void main(String[] args) {
+    }
+}
